ba4b1b85c3d94eca76ab0e68581fa658bc8fb5df
Manjunath <38625483+Manjunath1208@users.noreply.github.com>
2021-05-11 19:47:50 +0530

    Delete Cart module/cartmodule directory

--- Cart module/cartmodule/src/components/login.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Login = () => {
-    return (<div>
-        User Login Module
-    </div>);
-}
-
-export default Login;
--- Cart module/cartmodule/src/components/register.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Register = () => {
-    return (<div>
-        <h1>User Register Module</h1>
-    </div>);
-}
-
-export default Register;
